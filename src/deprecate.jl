bbb587c514abbcdb4092f6b6dea008185061517f
mauro-milella <milella_mauro@live.it>
2023-09-13 10:22:20 +0200

    deprecate.jl update

--- src/deprecate.jl
+++ src/deprecate.jl
@@ -1,10 +1,12 @@
-Base.@deprecate function check(
+export global_diamond, global_box
+
+Base.@deprecate check(
     φ::SoleLogics.AbstractFormula,
     X::AbstractInterpretationSet{<:AbstractKripkeStructure},
     i_instance::Integer,
     args...;
     kwargs...
-)
+) check(tree(φ), X, i_instance, args...; kwargs...)
 
-Base.@deprecate globaldiamond
-Base.@deprecate globalbox
+const global_diamond = globaldiamond
+const global_box = globalbox
