cb6909275e009e2878e361767aa5934442738914
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-10-03 15:51:49 +0200

    Minor

--- src/modal-logic.jl
+++ src/modal-logic.jl
@@ -498,7 +498,7 @@ include("algebras/frames.jl")
 # """
 # TODO
 # """
-# function check(::Atom{A}, ::AbstractModalAssignment{W,A,T}, ::W)::T where {W<:AbstractWorld,A,T<:TruthValue}
+# function check(::Atom{A}, ::AbstractModalAssignment{W,A,T}, ::W) where {W<:AbstractWorld,A,T<:TruthValue}
 #     return error("Please, provide ...")
 # end
 
