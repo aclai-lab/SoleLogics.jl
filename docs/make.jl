9b26e07784578cd0ffe04f095c8b7aaa1560d751
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-11-11 14:52:58 +1100

    Fix docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,16 +3,13 @@ using Documenter
 
 DocMeta.setdocmeta!(SoleLogics, :DocTestSetup, :(using SoleLogics); recursive = true)
 
-Documenter.HTMLWriter.HTML(
-    size_threshold = 4000000,
-)
-
 makedocs(;
     modules = [SoleLogics],
     authors = "Mauro Milella, Giovanni Pagliarini, Eduard I. Stan",
-    repo = "https://github.com/aclai-lab/SoleLogics.jl/blob/{commit}{path}#{line}",
+    repo=GitHub("aclai-lab", "SoleLogics.jl"),
     sitename = "SoleLogics.jl",
     format = Documenter.HTML(;
+        size_threshold = 4000000,
         prettyurls = get(ENV, "CI", "false") == "true",
         canonical = "https://aclai-lab.github.io/SoleLogics.jl",
         assets = String[],
@@ -22,9 +19,11 @@ makedocs(;
         "Getting started" => "getting-started.md",
         "Propositional logic" => "propositional-logic.md",
         "Modal logic" => "modal-logic.md",
-        # "Full reference" => "autodocs.md"
+        "Full reference" => "autodocs.md"
     ]
 )
+
+@info "`makedocs` has finished running. "
 
 deploydocs(;
     repo = "github.com/aclai-lab/SoleLogics.jl",
