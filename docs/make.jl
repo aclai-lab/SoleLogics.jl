98bad997dad00eb68a67343e8b38f35b2b942840
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-11-11 14:45:49 +1100

    Fix docs maybe

--- docs/make.jl
+++ docs/make.jl
@@ -22,7 +22,7 @@ makedocs(;
         "Getting started" => "getting-started.md",
         "Propositional logic" => "propositional-logic.md",
         "Modal logic" => "modal-logic.md",
-        "Full reference" => "autodocs.md"
+        # "Full reference" => "autodocs.md"
     ]
 )
 
