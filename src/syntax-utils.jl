62ce4b093925739059ffad47265311699fe94de4
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-12-05 22:01:00 +1100

    Improve docstrings

--- src/syntax-utils.jl
+++ src/syntax-utils.jl
@@ -476,20 +476,32 @@ end
 """
     normalize(
         f::Formula;
-        remove_boxes = true,
+        profile = :readability,
+        remove_boxes = nothing,
         reduce_negations = true,
-        allow_atom_flipping = true
+        simplify_constants = true,
+        allow_atom_flipping = false,
+        prefer_implications = false,
+        remove_implications = false,
+        forced_negation_removal = nothing,
+        remove_identities = true,
+        unify_toones = true,
+        rotate_commutatives = true,
     )
 
 Return a modified version of a given formula, that has the same semantics
-but different syntax. This is useful when dealing with the truth of many
-(possibly similar) formulas; for example, when performing
+but different syntax. This is useful for simplifying formulas for readability,
+or when checking the truth of many
+(possibly semantically similar) formulas; for example, when performing
 [model checking](https://en.wikipedia.org/wiki/Model_checking).
-BEWARE: it currently assumes the underlying algebra is Boolean!
+The current implementation assumes the underlying algebra is Boolean!
 
 # Arguments
 - `f::Formula`: when set to `true`,
     the formula;
+- `profile::Symbol`: possible values are :readability, which optimizes for qualitative
+    simplicity for a human to understand, and :modelchecking, which optimizes
+    model checking speed;
 - `remove_boxes::Bool`: remove all (non-relational and relational) box operators by using the
     equivalence ◊φ ≡ ¬□¬φ. Note: this assumes an underlying Boolean algebra.
 - `reduce_negations::Bool`: when set to `true`,
@@ -531,7 +543,7 @@ function normalize(
     forced_negation_removal = nothing,
     remove_identities = nothing,
     unify_toones = nothing,
-    rotate_commutatives = nothing
+    rotate_commutatives = nothing,
 )
     if profile == :readability
         if isnothing(remove_boxes)               remove_boxes = false end
