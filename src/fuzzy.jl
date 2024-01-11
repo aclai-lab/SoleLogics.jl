a17166c2aeb55cd25f2e4969c3c9d019def90945
alberto-paparella <alberto.paparella@edu.unife.it>
2024-01-11 16:24:11 +0100

    Added cast from BooleanTruth to HeytingTruth

--- src/fuzzy.jl
+++ src/fuzzy.jl
@@ -8,11 +8,25 @@ end
 label(heytingtruth::HeytingTruth)::String = heytingtruth.label
 index(heytingtruth::HeytingTruth)::Int = heytingtruth.index
 
+convert(::Type{HeytingTruth}, booleantruth::BooleanTruth) = istop(booleantruth) ? HeytingTruth("⊤", 2) : HeytingTruth("⊥", 1)
+
 struct HeytingAlgebra
     domain::Vector{HeytingTruth}
     top::HeytingTruth
     bot::HeytingTruth
     graph::Graphs.SimpleGraphs.SimpleDiGraph   # directed graph where each edge (α, β) is consistend with α ≺ β
+
+    function HeytingAlgebra(domain::Vector{HeytingTruth}, top::HeytingTruth, bot::HeytingTruth, graph::Graphs.SimpleGraphs.SimpleDiGraph)
+        return new(domain, top, bot, graph)
+    end
+
+    function HeytingAlgebra(domain::Vector{HeytingTruth}, graph::Graphs.SimpleGraphs.SimpleDiGraph)
+        return HeytingAlgebra(domain, convert(HeytingTruth, ⊤), convert(HeytingTruth, ⊥), graph)
+    end
+
+    function HeytingAlgebra(domain::Vector{HeytingTruth}, relations::Vector{Edge{Int64}})
+        return HeytingAlgebra(domain, convert(HeytingTruth, ⊤), convert(HeytingTruth, ⊥), SimpleDiGraph(relations))
+    end
 end
 
 domain(h::HeytingAlgebra) = h.domain
@@ -20,31 +34,32 @@ top(h::HeytingAlgebra) = h.top
 bot(h::HeytingAlgebra) = h.bot
 graph(h::HeytingAlgebra) = h.graph
 
+"""
+⊥ and ⊤ already exist as const of type BooleanTruth and they are treated as HeytingTruth with index 1 and 2 respectively
+"""
 macro heytingtruths(labels...)
     quote
-        $(map(t -> :(const $(t[2]) = $(HeytingTruth(string(t[2]), t[1]))), enumerate(labels))...)
+        $(map(t -> :(const $(t[2]) = $(HeytingTruth(string(t[2]), t[1]+2))), enumerate(labels))...)
         [$(labels...)]
     end |> esc
 end
 
 """
-TODO: heytingalgebra(name, domain) and heytingalgebra(name, domain, relations...) macros
+Note that values of type HeytingTruth must be created beforehand (e.g., with @heytingvalues values...) and not include ⊥ and ⊤
 """
-macro heytingalgebra(name, relations...)
+macro heytingalgebra(name, values, relations...)
     quote
-        $(labelsset = Set{Symbol}())
-        $(map(t -> map(s -> push!(labelsset, s), t.args), relations))
-        $(labels = collect(labelsset))
-        @heytingtruths $(labels...)     
-        domain = [$(labels...)]
+        domain = [convert(HeytingTruth, ⊥), convert(HeytingTruth, ⊤), $values...]
         edges = Vector{Edge{Int64}}()
         map(e -> push!(edges, Edge(eval(e))), $relations)
-        graph = SimpleDiGraph(edges)
-        const $name = (HeytingAlgebra(domain, ⊤0, ⊥0, graph))
+        const $name = (HeytingAlgebra(domain, edges))
     end |> esc
 end
 
 Graphs.Edge(t::Tuple{HeytingTruth, HeytingTruth}) = Edge(index(t[1]), index(t[2]))
+Graphs.Edge(t::Tuple{HeytingTruth, BooleanTruth}) = Edge((t[1], convert(HeytingTruth, t[2])))
+Graphs.Edge(t::Tuple{BooleanTruth, HeytingTruth}) = Edge((convert(HeytingTruth, t[1]), t[2]))
+Graphs.Edge(t::Tuple{BooleanTruth, BooleanTruth}) = Edge((convert(HeytingTruth, t[1]), convert(HeytingTruth, t[2])))
 
 function Graphs.inneighbors(heytingalgebra::HeytingAlgebra, heytingtruth::HeytingTruth)::Vector{HeytingTruth}
     return domain(heytingalgebra)[inneighbors(graph(heytingalgebra), index(heytingtruth))]
@@ -69,14 +84,33 @@ function precedes(h::HeytingAlgebra, α::HeytingTruth, β::HeytingTruth)
         return false
     end
 end
+precedes(h::HeytingAlgebra, α::HeytingTruth, β::BooleanTruth) = precedes(h, α, convert(HeytingTruth, β))
+precedes(h::HeytingAlgebra, α::BooleanTruth, β::HeytingTruth) = precedes(h, convert(HeytingTruth, α), β)
+precedes(h::HeytingAlgebra, α::BooleanTruth, β::BooleanTruth) = precedes(h, convert(HeytingTruth, α), convert(HeytingTruth, β))
 
 """
 β ≺ α
 """
 succeedes(h::HeytingAlgebra, α::HeytingTruth, β::HeytingTruth) = precedes(h, β, α)
+succeedes(h::HeytingAlgebra, α::HeytingTruth, β::BooleanTruth) = succeedes(h, α, convert(HeytingTruth, β))
+succeedes(h::HeytingAlgebra, α::BooleanTruth, β::HeytingTruth) = succeedes(h, convert(HeytingTruth, α), β)
+succeedes(h::HeytingAlgebra, α::BooleanTruth, β::BooleanTruth) = succeedes(h, convert(HeytingTruth, α), convert(HeytingTruth, β))
 
+"""
+α ⪯ β
+"""
 precedeq(h::HeytingAlgebra, α::HeytingTruth, β::HeytingTruth) = α == β ||  precedes(h, α, β)
+precedeq(h::HeytingAlgebra, α::HeytingTruth, β::BooleanTruth) = precedeq(h, α, convert(HeytingTruth, β))
+precedeq(h::HeytingAlgebra, α::BooleanTruth, β::HeytingTruth) = precedeq(h, convert(HeytingTruth, α), β)
+precedeq(h::HeytingAlgebra, α::BooleanTruth, β::BooleanTruth) = precedeq(h, convert(HeytingTruth, α), convert(HeytingTruth, β))
+
+"""
+β ⪯ α
+"""
 succeedeq(h::HeytingAlgebra, α::HeytingTruth, β::HeytingTruth) = α == β ||  succeedes(h, α, β)
+succeedeq(h::HeytingAlgebra, α::HeytingTruth, β::BooleanTruth) = succeedeq(h, α, convert(HeytingTruth, β))
+succeedeq(h::HeytingAlgebra, α::BooleanTruth, β::HeytingTruth) = succeedeq(h, convert(HeytingTruth, α), β)
+succeedeq(h::HeytingAlgebra, α::BooleanTruth, β::BooleanTruth) = succeedeq(h, convert(HeytingTruth, α), convert(HeytingTruth, β))
 
 """
 Meet (greatest lower bound) between values α and β
@@ -130,3 +164,7 @@ function collatetruth(::typeof(→), (α, β)::NTuple{N, T where T<:HeytingTruth
     end
     return η
 end
+
+collatetruth(c::Connective, (α, β)::Tuple{HeytingTruth, BooleanTruth}, h::HeytingAlgebra) = collatetruth(c, (α, convert(HeytingTruth, β)), h)
+collatetruth(c::Connective, (α, β)::Tuple{BooleanTruth, HeytingTruth}, h::HeytingAlgebra) = collatetruth(c, (convert(HeytingTruth, α), β), h)
+collatetruth(c::Connective, (α, β)::Tuple{BooleanTruth, BooleanTruth}, h::HeytingAlgebra) = collatetruth(c, (convert(HeytingTruth, α), convert(HeytingTruth, β)), h)
--- test/fuzzy.jl
+++ test/fuzzy.jl
@@ -1,70 +1,71 @@
 using SoleLogics
 using Graphs
 
-@heytingalgebra heytingalgebra4 (⊥0, α) (⊥0, β) (α, ⊤0) (β, ⊤0)
+@heytingtruths α β
+@heytingalgebra heytingalgebra4 (α, β) (⊥, α) (⊥, β) (α, ⊤) (β, ⊤)
 
-@test precedes(heytingalgebra4, ⊥0, α)   == true
-@test precedes(heytingalgebra4, α, ⊥0)   == false
-@test precedes(heytingalgebra4, ⊥0, β)   == true
-@test precedes(heytingalgebra4, β, ⊥0)   == false
-@test precedes(heytingalgebra4, ⊥0, ⊤0)   == true
-@test precedes(heytingalgebra4, ⊤0, ⊥0)   == false
+@test precedes(heytingalgebra4, ⊥, α)   == true
+@test precedes(heytingalgebra4, α, ⊥)   == false
+@test precedes(heytingalgebra4, ⊥, β)   == true
+@test precedes(heytingalgebra4, β, ⊥)   == false
+@test precedes(heytingalgebra4, ⊥, ⊤)   == true
+@test precedes(heytingalgebra4, ⊤, ⊥)   == false
 @test precedes(heytingalgebra4, α, β)   == false
 @test precedes(heytingalgebra4, β, α)   == false
-@test precedes(heytingalgebra4, α, ⊤0)   == true
-@test precedes(heytingalgebra4, ⊤0, α)   == false
-@test precedes(heytingalgebra4, β, ⊤0)   == true
-@test precedes(heytingalgebra4, ⊤0, β)   == false
+@test precedes(heytingalgebra4, α, ⊤)   == true
+@test precedes(heytingalgebra4, ⊤, α)   == false
+@test precedes(heytingalgebra4, β, ⊤)   == true
+@test precedes(heytingalgebra4, ⊤, β)   == false
 
-@test succeedes(heytingalgebra4, ⊥0, α)  == false
-@test succeedes(heytingalgebra4, α, ⊥0)  == true
-@test succeedes(heytingalgebra4, ⊥0, β)  == false
-@test succeedes(heytingalgebra4, β, ⊥0)  == true
-@test succeedes(heytingalgebra4, ⊥0, ⊤0)  == false
-@test succeedes(heytingalgebra4, ⊤0, ⊥0)  == true
+@test succeedes(heytingalgebra4, ⊥, α)  == false
+@test succeedes(heytingalgebra4, α, ⊥)  == true
+@test succeedes(heytingalgebra4, ⊥, β)  == false
+@test succeedes(heytingalgebra4, β, ⊥)  == true
+@test succeedes(heytingalgebra4, ⊥, ⊤)  == false
+@test succeedes(heytingalgebra4, ⊤, ⊥)  == true
 @test succeedes(heytingalgebra4, α, β)  == false
 @test succeedes(heytingalgebra4, β, α)  == false
-@test succeedes(heytingalgebra4, α, ⊤0)  == false
-@test succeedes(heytingalgebra4, ⊤0, α)  == true
-@test succeedes(heytingalgebra4, β, ⊤0)  == false
-@test succeedes(heytingalgebra4, ⊤0, β)  == true
+@test succeedes(heytingalgebra4, α, ⊤)  == false
+@test succeedes(heytingalgebra4, ⊤, α)  == true
+@test succeedes(heytingalgebra4, β, ⊤)  == false
+@test succeedes(heytingalgebra4, ⊤, β)  == true
 
-@test collatetruth(∧, (⊥0, α), heytingalgebra4)  == ⊥0
-@test collatetruth(∧, (α, ⊥0), heytingalgebra4)  == ⊥0
-@test collatetruth(∧, (⊥0, β), heytingalgebra4)  == ⊥0
-@test collatetruth(∧, (β, ⊥0), heytingalgebra4)  == ⊥0
-@test collatetruth(∧, (⊥0, ⊤0), heytingalgebra4)  == ⊥0
-@test collatetruth(∧, (⊤0, ⊥0), heytingalgebra4)  == ⊥0
-@test collatetruth(∧, (α, β), heytingalgebra4)  == ⊥0
-@test collatetruth(∧, (β, α), heytingalgebra4)  == ⊥0
-@test collatetruth(∧, (α, ⊤0), heytingalgebra4)  == α
-@test collatetruth(∧, (⊤0, α), heytingalgebra4)  == α
-@test collatetruth(∧, (β, ⊤0), heytingalgebra4)  == β
-@test collatetruth(∧, (⊤0, β), heytingalgebra4)  == β
+@test collatetruth(∧, (⊥, α), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∧, (α, ⊥), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∧, (⊥, β), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∧, (β, ⊥), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∧, (⊥, ⊤), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∧, (⊤, ⊥), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∧, (α, β), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∧, (β, α), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∧, (α, ⊤), heytingalgebra4)  == α
+@test collatetruth(∧, (⊤, α), heytingalgebra4)  == α
+@test collatetruth(∧, (β, ⊤), heytingalgebra4)  == β
+@test collatetruth(∧, (⊤, β), heytingalgebra4)  == β
 
-@test collatetruth(∨, (⊥0, ⊥0), heytingalgebra4)  == ⊥0
-@test collatetruth(∨, (⊥0, α), heytingalgebra4)  == α
-@test collatetruth(∨, (α, ⊥0), heytingalgebra4)  == α
-@test collatetruth(∨, (⊥0, β), heytingalgebra4)  == β
-@test collatetruth(∨, (β, ⊥0), heytingalgebra4)  == β
-@test collatetruth(∨, (⊥0, ⊤0), heytingalgebra4)  == ⊤0
-@test collatetruth(∨, (⊤0, ⊥0), heytingalgebra4)  == ⊤0
-@test collatetruth(∨, (α, β), heytingalgebra4)  == ⊤0
-@test collatetruth(∨, (β, α), heytingalgebra4)  == ⊤0
-@test collatetruth(∨, (α, ⊤0), heytingalgebra4)  == ⊤0
-@test collatetruth(∨, (⊤0, α), heytingalgebra4)  == ⊤0
-@test collatetruth(∨, (β, ⊤0), heytingalgebra4)  == ⊤0
-@test collatetruth(∨, (⊤0, β), heytingalgebra4)  == ⊤0
+@test collatetruth(∨, (⊥, ⊥), heytingalgebra4)  == convert(HeytingTruth, ⊥)
+@test collatetruth(∨, (⊥, α), heytingalgebra4)  == α
+@test collatetruth(∨, (α, ⊥), heytingalgebra4)  == α
+@test collatetruth(∨, (⊥, β), heytingalgebra4)  == β
+@test collatetruth(∨, (β, ⊥), heytingalgebra4)  == β
+@test collatetruth(∨, (⊥, ⊤), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(∨, (⊤, ⊥), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(∨, (α, β), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(∨, (β, α), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(∨, (α, ⊤), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(∨, (⊤, α), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(∨, (β, ⊤), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(∨, (⊤, β), heytingalgebra4)  == convert(HeytingTruth, ⊤)
 
-@test collatetruth(→, (⊥0, α), heytingalgebra4)  == ⊤0
-@test collatetruth(→, (α, ⊥0), heytingalgebra4)  == β
-@test collatetruth(→, (⊥0, β), heytingalgebra4)  == ⊤0
-@test collatetruth(→, (β, ⊥0), heytingalgebra4)  == α
-@test collatetruth(→, (⊥0, ⊤0), heytingalgebra4)  == ⊤0
-@test collatetruth(→, (⊤0, ⊥0), heytingalgebra4)  == ⊥0
+@test collatetruth(→, (⊥, α), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(→, (α, ⊥), heytingalgebra4)  == β
+@test collatetruth(→, (⊥, β), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(→, (β, ⊥), heytingalgebra4)  == α
+@test collatetruth(→, (⊥, ⊤), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(→, (⊤, ⊥), heytingalgebra4)  == convert(HeytingTruth, ⊥)
 @test collatetruth(→, (α, β), heytingalgebra4)  == β
 @test collatetruth(→, (β, α), heytingalgebra4)  == α
-@test collatetruth(→, (α, ⊤0), heytingalgebra4)  == ⊤0
-@test collatetruth(→, (⊤0, α), heytingalgebra4)  == α
-@test collatetruth(→, (β, ⊤0), heytingalgebra4)  == ⊤0
-@test collatetruth(→, (⊤0, β), heytingalgebra4)  == β
+@test collatetruth(→, (α, ⊤), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(→, (⊤, α), heytingalgebra4)  == α
+@test collatetruth(→, (β, ⊤), heytingalgebra4)  == convert(HeytingTruth, ⊤)
+@test collatetruth(→, (⊤, β), heytingalgebra4)  == β
