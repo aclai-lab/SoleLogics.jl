f40ca19defc5090ee973d4b6fd0af88a1aa123a8
alberto-paparella <alberto.paparella@edu.unife.it>
2024-03-12 18:18:03 +0100

    Added _accessibles for IA_Ai IntervalLengthFilter

--- src/algebras/frames/full-dimensional-frame/FullDimensionalFrame-filtered.jl
+++ src/algebras/frames/full-dimensional-frame/FullDimensionalFrame-filtered.jl
@@ -9,3 +9,15 @@ end
 function _accessibles(fr::Full1DFrame, w::W, r::FilteredRelation{_IA_A,IntervalLengthFilter{typeof(==),T,W}}) where {T<:Real,W<:Interval{Int}}
 	return zip(Iterators.repeated(w.y), w.y+r.wf.k:min(w.y+r.wf.k,X(fr)+1))
 end
+
+function _accessibles(fr::Full1DFrame, w::W, r::FilteredRelation{_IA_Ai,IntervalLengthFilter{typeof(≤),T,W}}) where {T<:Real,W<:Interval{Int}}
+	return zip(max(w.x-r.wf.k,1):w.x-1, Iterators.repeated(w.x))
+end
+
+function _accessibles(fr::Full1DFrame, w::W, r::FilteredRelation{_IA_Ai, IntervalLengthFilter{typeof(≥),T,W}}) where {T<:Real,W<:Interval{Int}}
+	return zip(1:w.x-r.wf.k, Iterators.repeated(w.x))
+end
+
+function _accessibles(fr::Full1DFrame, w::W, r::FilteredRelation{_IA_Ai,IntervalLengthFilter{typeof(==),T,W}}) where {T<:Real,W<:Interval{Int}}
+	return zip(max(w.x-r.wf.k,1):w.x-r.wf.k, Iterators.repeated(w.x))
+end
