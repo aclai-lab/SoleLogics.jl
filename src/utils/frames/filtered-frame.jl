d464b2b424d39e2da0061b1216d013d7aedaf8af
giopaglia <24519853+giopaglia@users.noreply.github.com>
2024-12-28 18:41:21 +0100

    Minor

--- src/utils/frames/filtered-frame.jl
+++ src/utils/frames/filtered-frame.jl
@@ -10,15 +10,15 @@ A (binary) accessibility relation `r`, filtered by a world filter `wf`.
 struct FilteredFrame{W<:AbstractWorld,F<:AbstractMultiModalFrame{W},WF<:WorldFilter{W}} <: AbstractMultiModalFrame{W}
     r::F
     wf::WF
-
+    
     function FilteredFrame{W,F,WF}(r::F, wf::WF) where {W<:AbstractWorld,F<:AbstractMultiModalFrame{W},WF<:WorldFilter{W}}
         return new{W,F,WF}(r, wf)
     end
-
+    
     function FilteredFrame(r::F, wf::WF) where {W<:AbstractWorld,F<:AbstractMultiModalFrame{W},WF<:WorldFilter{W}}
         return FilteredFrame{W,F,WF}(r, wf)
     end
-
+    
     # TODO constructor that accepts a Callable and wraps it into a FunctionalWorldFilter?
 end
 
