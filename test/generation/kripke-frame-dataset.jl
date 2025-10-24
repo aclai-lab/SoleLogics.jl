1a1e6cbae795d4ecfa32425bea0c22fae8c46c4b
mauro-milella <milella_mauro@live.it>
2025-10-24 19:43:23 +0200

    [ADD] spartacustomodel (to be tested)

--- test/generation/kripke-frame-dataset.jl
+++ test/generation/kripke-frame-dataset.jl
@@ -2,20 +2,16 @@
 # in particular, we want to leverage Spartacus (already installed by SoleReasoners)
 # to propose models (if there are any).
 
+using Graphs
+
 # we exploit SoleReasoners (the embedding branch, at the moment of reading)
 using SoleLogics
+
 
 """
 Install spartacus solver in a subfolder at the location of this file.
 
-The installer is:
-```
-cd $1
-curl https://www.ps.uni-saarland.de/spartacus/spartacus-1.1.3.tar.bz2 -o spartacus-1.1.2.tar.bz2
-tar -x -f spartacus-1.1.2.tar.bz2
-cd spartacus
-make spartacus
-```
+See install-spartacus.sh script.
 
 You need mlton; you can get it from https://sourceforge.net/projects/mlton/files/mlton/20210117/mlton-20210117-1.amd64-linux-glibc2.31.tgz
 (adjust for your architecture and OS) and tar xfv the resulting folder;
@@ -100,7 +96,103 @@ _spartan_conjunction = soletospartacus(_conjunction)
 _command = `$(SPARTACUS_DIR)/spartacus --showModel --formula=$(_spartan_conjunction)`
 
 _buffer = IOBuffer()
-_raw_result = run(pipeline(cmd, stdout=_buffer))
+_raw_result = run(pipeline(_command, stdout=_buffer))
 _result = String(take!(_buffer))
 
 # now, we need to parse the _result
+"""
+    function spartacustomodel(
+        spartacuslog::String;
+        atomsconvertion::Function=identity
+    )::KripkeStructure
+
+
+By @perro2110 and @mauro_milella.
+"""
+function spartacustomodel(
+    spartacuslog::String;
+    atomsconvertion::Function=identity
+)::KripkeStructure
+    if !isnothing(findfirst("unsatisfiable", spartacuslog))
+        throw(ArgumentError("Error: the provided spartacus model is unsatisfiable."))
+    end
+
+    # example after splitting:
+    # -----------------------------------------------------
+    # 3 nodes
+    # ------------------------------------------------------------
+    # Node 0
+    # propositions: p, q, r, s, t, u, w, y, z
+    # successors:   a:1
+    # ------------------------------------------------------------
+    # Node 1
+    # propositions: r, u
+    # successors:   a:2
+    # ------------------------------------------------------------
+    # Node 2
+    # propositions: p
+    # successors:
+    # ------------------------------------------------------------
+    spartacuslog = split(spartacuslog, "MODEL")[2]
+
+    # now, we split by \n
+    spartacuslog = split(spartacuslog, "\n")
+    nworlds = parse(Int, split(spartacuslog[2], " ")[1])
+
+    # we can discard the first 3 lines
+    spartacuslog = spartacuslog[3:end]
+
+    # the graph is a directed graph; at the moment, we only know the numberf of its vertices
+    graph = Graphs.SimpleDiGraph(nworlds)
+
+    # keep track of the atoms that are true on the ith world/vertex
+    ithworld_to_atoms = Dict{Int,Vector{Atom}}()
+    # also, we want to keep track of all the existent atoms
+    allatoms = Atom[]
+
+    for i in 1:nworlds
+        # "Node 1" becomes 1 (as integer)
+        currentworld = parse(Int, split(spartacuslog[i+1], " ")[end])
+
+        # "propositions: r, u" becomes [r,u] (whose type is defined by atomsconvertion)
+        atoms_on_currentworld = split(spartacuslog[i+2], " ")[2:end] .|> String
+        atoms_on_currentworld = filter.(
+            x -> x != ',', atoms_on_currentworld) .|> identity .|> Atom
+
+        # keep track of the linking between a specific world and its atoms
+        ithworld_to_atoms[currentworld] = atoms_on_currentworld
+
+        # later, by applying unique to this collection, we will isolate the entire alphabet
+        push!(allatoms, atoms_on_currentworld...)
+
+        # "successors:   a:1, a:2" becomes
+        successors = split(spartacuslog[i+3], "successors:")[2] .|> strip
+        for (_,targetworld) in split.(successors, ":")
+            # WARNING: beware, as the specific type of relations is currently ignored
+            push!(graph, currentworld, targetworld)
+        end
+    end
+
+    # allatoms now contains the full alphabet
+    unique!(allatoms)
+
+    # translate from Graphs to Kripke frames
+    worlds = World.(1:nworlds)
+    _frame = SoleLogics.ExplicitCrispUniModalFrame(worlds, graph)
+
+    function _assign_truthvalues(allatoms::Vector{Atom}, ithworld_to_atoms::Vector{Atom})
+        atom_to_top = [_atom => TOP for _atom in ithworld_to_atoms]
+        atom_to_bot = [_atom => BOT for _atom in setdiff(allatoms, ithworld_to_atoms)]
+
+        return TruthDict(Iterators.flatten([atom_to_top, atom_to_bot]))
+    end
+
+    # for each world w that we collected,
+    # assign to it the atoms that are true (TOP) or false (BOT) on them
+    valuation = Dict([
+        w => _assign_truthvalues(allatoms, ithworld_to_atoms[wid])
+        for (wid,w) in enumerate(worlds)
+    ])
+
+    return KripkeStructure(_frame, valuation)
+end
