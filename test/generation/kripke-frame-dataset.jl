b5ff603d7acf998d2ac991b219fd14d9191d8e7e
mauro-milella <milella_mauro@live.it>
2025-10-24 14:00:46 +0200

    [ADD] installspartacus, soletospartacus (the latter is taken from SoleReasoners.jl)

---
+++ test/generation/kripke-frame-dataset.jl
@@ -0,0 +1,88 @@
+# we want to generate a set of Kripke frames, starting from a set of modal formulas;
+# in particular, we want to leverage Spartacus (already installed by SoleReasoners)
+# to propose models (if there are any).
+
+# we exploit SoleReasoners (the embedding branch, at the moment of reading)
+using SoleLogics
+
+"""
+Install spartacus solver in a subfolder at the location of this file.
+
+The installer is:
+```
+cd $1
+curl https://www.ps.uni-saarland.de/spartacus/spartacus-1.1.3.tar.bz2 -o spartacus-1.1.2.tar.bz2
+tar -x -f spartacus-1.1.2.tar.bz2
+cd spartacus
+make spartacus
+```
+
+You need mlton; you can get it from https://sourceforge.net/projects/mlton/files/mlton/20210117/mlton-20210117-1.amd64-linux-glibc2.31.tgz
+(adjust for your architecture and OS) and tar xfv the resulting folder;
+also, you need to move `bin` and `lib` folders to, respectively, `usr/bin` and `usr/lib`.
+"""
+function installspartacus()
+    if !isspartacusinstalled()
+        println("Installing Spartacus")
+        run(`sh $(joinpath(@__DIR__, "install_spartacus.sh")) $(@__DIR__)`)
+    end
+end
+
+
+"""
+Convert a sole formula to spartacus format.
+"""
+function soletospartacus(φ::Union{Atom, BooleanTruth, SyntaxBranch})
+    s = syntaxstring(φ)
+    r = Vector{Char}()
+    for c in s
+        if c == '¬'
+            push!(r, '~')
+        elseif c == '∧'
+            push!(r, '&')
+        elseif c == '∨'
+            push!(r, '|')
+        elseif c == '→'
+            push!(r, '-', '>')
+        elseif c == '◊'
+            push!(r, '<', 'a', '>')
+        elseif c == '□'
+            push!(r, '[', 'a', ']')
+        elseif c == '⊤'
+            push!(r, '1')
+        elseif c == '⊥'
+            push!(r, '0')
+        else
+            push!(r, c)
+        end
+    end
+    return String(r)
+end
+
+
+WORKING_DIR = joinpath(@__DIR__, "test", "generation")
+
+_myrng = 42
+_alphabet = ExplicitAlphabet(Atom.('a':'f'))
+_operators = [CONJUNCTION, DISJUNCTION, NEGATION, DIAMOND, BOX]
+
+_earlystoppingthreshold = 0.2
+_nformulas = 10
+
+
+_formulas = [
+    randformula(
+        _myrng,
+        _alphabet,
+        _operators;
+        earlystoppingthreshold =_earlystoppingthreshold
+    )
+    for _ in 1:_nformulas
+]
+
+# we want a model that satisfies all the generated formulas;
+# we will leverage soletospartacus
+soletospartacus(CONJUNCTION(_formulas...))
+
+# invoke executable
+run(joinpath(WORKING_DIR, "spartacus", "bin"))
