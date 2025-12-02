cf2def760e19a9590a7cbc1f3ba0cbba520945d2
mauro-milella <milella_mauro@live.it>
2025-12-02 22:57:55 +0100

    [ADD] op weights

--- test/generation/logiset.jl
+++ test/generation/logiset.jl
@@ -1,18 +1,20 @@
 
 @testset "randlogiset" begin
 
-_myrng = 42
+_myrng = 238
 _alphabet = ExplicitAlphabet(Atom.('p':'z'))
 _operators = [CONJUNCTION, DISJUNCTION, NEGATION, DIAMOND, BOX]
+_operator_weights = [1, 2, 2, 5, 1]
 
-_nformulas = 10
+_nformulas = 5
 
-_earlystoppingthreshold = 0.1
+_earlystoppingthreshold = 0.3
 _formulas = [
     randformula(
         _myrng,
         _alphabet,
         _operators;
+        opweights=_operator_weights,
         earlystoppingthreshold =_earlystoppingthreshold
     )
     for _ in 1:_nformulas
@@ -20,6 +22,6 @@ _formulas = [
 
 _conjunction = CONJUNCTION(_formulas...)
 
-mylogiset = randlogiset(_myrng, ((_conjunction,)), 5; silent=false, checksat=true)
+mylogisets, _ = randlogiset(_myrng, ((_conjunction,)), 5; silent=false, checksat=true)
 
 end
